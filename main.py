5a00176448c1e9ed976481a851e7be32ba678e28
Eliecer Santana Rodriguez <eliecer.santana@uip.edu.pa>
2026-07-12 15:27:07 -0500

    Agregar edad y ciudad al programa

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # informacion de nombre
 nombre = input("Por favor, ingresa tu nombre: ")
 
+edad = input ("Por favor, ingrese tu edad: ")
+ciudad = input("Por favor, ingrese tu ciudad: ")
+
 print("¡Hola que tal,", nombre)
+print("Tienes", edad, "años y vives en ", ciudad + ".")
