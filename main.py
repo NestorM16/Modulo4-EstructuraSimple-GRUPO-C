aa29fa33b734f39e0a868f057f7bd9f74351737a
Eliecer Santana Rodriguez <eliecer.santana@uip.edu.pa>
2026-07-12 15:59:05 -0500

    agregue edad

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 nombre = input("Por favor, ingresa tu nombre: ")
 
 edad = input ("Por favor, ingrese tu edad: ")
-ciudad = input("Por favor, ingrese tu ciudad: ")
 
 print("¡Hola que tal,", nombre)
-print("Tienes", edad, "años y vives en ", ciudad + ".")
+print("Tu edad es de", edad, "años.")
