ea298598a4d399603dad71c81113f555f9475400
Eliecer Santana Rodriguez <eliecer.santana@uip.edu.pa>
2026-07-13 08:27:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ nombre = input("Por favor, ingresa tu nombre: ")
 
 edad = input ("Por favor, ingrese tu edad: ")
 
-print("¡Hola que tal,", nombre)
+print("¡Hola que tal,", nombre, "tu edad es de", edad)
